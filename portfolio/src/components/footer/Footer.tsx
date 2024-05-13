c013896c36a58f4dc3114e10bfe944487b61de4b
ksarson <37591778+ksarson@users.noreply.github.com>
2024-05-13 13:14:58 -0400

    Changed Colour Scheme, Completed About Me Text

--- portfolio/src/components/footer/Footer.tsx
+++ portfolio/src/components/footer/Footer.tsx
@@ -1,7 +1,25 @@
 import '../../styles/Footer.scss';
 
 const Footer = () => {
-    return <footer>Footer</footer>;
+    return (
+        <footer>
+            <ul>
+                <li>
+                    <a href="#github">GitHub</a>
+                </li>
+                <li>
+                    <a href="#linkedin">LinkedIn</a>
+                </li>
+                <li>
+                    <a href="#email">Email</a>
+                </li>
+                <li>
+                    <a href="#resume">Resume</a>
+                </li>
+            </ul>
+            <div className="copyright">Copyright Ⓒ 2024 Kolby Sarson</div>
+        </footer>
+    );
 };
 
 export default Footer;
--- portfolio/src/components/main/section_AboutMe/AboutMe.tsx
+++ portfolio/src/components/main/section_AboutMe/AboutMe.tsx
@@ -1,7 +1,29 @@
 import '../../../styles/AboutMe.scss';
 
 const AboutMe = () => {
-    return <section className="about-me">About Me</section>;
+    return (
+        <section className="about-me">
+            <div>
+                Hello! I'm Kolby, a motivated
+                <span className="highlight"> full-stack developer</span> with
+                both a Bachelor's and Master's degree in Computer Science. With
+                over a year of hands-on experience in the{' '}
+                <span className="highlight">.NET/React</span> stack, I've
+                developed a knack for crafting{' '}
+                <span className="highlight">elegant solutions </span> to
+                <span className="highlight"> complex problems</span>. I thrive
+                in dynamic environments, where my strong{' '}
+                <span className="highlight">communication skills</span> and
+                passion for <span className="highlight">collaboration</span>{' '}
+                shine. Eager to embark on the next chapter of my software
+                engineering journey, I'm excited to leverage my expertise to
+                contribute to{' '}
+                <span className="highlight">impactful projects</span> and drive{' '}
+                <span className="highlight">innovation</span> in the tech
+                industry.
+            </div>
+        </section>
+    );
 };
 
 export default AboutMe;
