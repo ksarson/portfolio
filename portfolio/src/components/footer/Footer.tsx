67e603bd7385b524f3eed3e09720bab1105118f7
ksarson <37591778+ksarson@users.noreply.github.com>
2024-10-10 23:57:21 -0400

    Links open in new tab

--- portfolio/src/components/footer/Footer.tsx
+++ portfolio/src/components/footer/Footer.tsx
@@ -8,16 +8,36 @@ const Footer = () => {
             <img src={Logo} alt="Logo" className="footer-logo" />
             <ul>
                 <li>
-                    <a href="https://github.com/ksarson">GitHub</a>
+                    <a
+                        href="https://github.com/ksarson"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        GitHub
+                    </a>
                 </li>
                 <li>
-                    <a href="https://www.linkedin.com/in/sarsonk/">LinkedIn</a>
+                    <a
+                        href="https://www.linkedin.com/in/sarsonk/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        LinkedIn
+                    </a>
                 </li>
                 <li>
-                    <a href="mailto:ksarson10@gmail.com">Email</a>
+                    <a
+                        href="mailto:ksarson10@gmail.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Email
+                    </a>
                 </li>
                 <li>
-                    <a href={resume}>Resumé</a>
+                    <a href={resume} target="_blank" rel="noopener noreferrer">
+                        Resumé
+                    </a>
                 </li>
             </ul>
             <div className="copyright">Copyright Ⓒ 2024 Kolby Sarson</div>
